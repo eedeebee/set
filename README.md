c0493bb36b3a1ba28d9eae1e630e64aade3fb22d
Eric Bloch <eric.bloch@gmail.com>
2015-07-14 22:58:30 -0700

    Get basics working

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-set
+# Logic
+
+1. Start a game
+- Shuffle cards
+- Deal initial board - spots 1 thru 12
+- Player says set and touches 3 cards
+	- If set, then give player a set then
+		 - If cards, add 3 more cards from deck.
+		 - If no more cards, end game.
+	- Else, subtract a set from player
+- Player says no sets.
+	- If no sets, then
+		- If cards, add 3 more cards from deck.
+		- If no more cards, end game.
+	- If there is a set, say no. 
+
+# Objects
+
+## Game
+
+- Functions
+	- start
+	- quit
+	- end
+- Has
+	- cards
+	- board
+	- players
+
+## Card
+
+- Shape
+- Color
+- Number
+- Fill
+
+## Board
+
+- Functions
+	- placeCard
+	- removeCard
+	- removeSet
+	- hasSet
+	- listCards 
+- Has
+    - Cards
+
+
+## Player
+
+- Function
+	- loseSet
+	
+- Has
+	- sets 
+	- name
--- set.py
+++ set.py
@@ -1,3 +1,8 @@
+from enum import Enum
+import random
+import sys
+from termcolor import colored
+
 
 """
     Game of Set
@@ -40,4 +45,133 @@ if ((all_same(cards, color) || all_different(cards, color)) &&
 
 
 """
+class Number(Enum):
+    one = 1
+    two = 2
+    three = 3
+class Fill(Enum):
+    solid = 1
+    linefilled = 2
+    empty = 3
+class Color(Enum):
+    green = 1
+    red = 2
+    blue = 3
+class Shape(Enum):
+    squiggle = 1
+    diamond = 2
+    oval = 3
 
+class Card:
+    def __init__(self, number, fill, color, shape):
+        self.color = color
+        self.number = number
+        self.fill = fill
+        self.shape = shape
+
+    def __str__(self):
+        shape = self.shape.name if self.number == Number.one else self.shape.name + 's'
+        return colored(" " .join([str(self.number.value), self.fill.name, shape]), self.color.name)
+
+    def isSet(a, b, c):
+
+        numberOK =  ((a.number == b.number == c.number) or (a.number != b.number and b.number != c.number and a.number != c.number))
+        fillOK =  ((a.fill == b.fill == c.fill) or (a.fill != b.fill and b.fill != c.fill and a.fill != c.fill))
+        colorOK =  ((a.color == b.color == c.color) or (a.color != b.color and b.color != c.color and a.color != c.color))
+        shapeOK =  ((a.shape == b.shape == c.shape) or (a.shape != b.shape and b.shape != c.shape and a.shape != c.shape))
+
+        return colorOK and fillOK and shapeOK and numberOK
+
+class Board:
+
+    def __init__(self):
+        self.cards = []
+        self.numCards = 0
+
+    def placeCard(self, card):
+        self.cards.append(card)
+        return 
+
+    def removeCard(self, index):
+        self.cards.pop(index)
+        return 
+
+    def removeSet(self, s):
+        if len(s) != 3:
+            return False
+
+        x = int(s[0]) - 1
+        y = int(s[1]) - 1
+        z = int(s[2]) - 1
+        if Card.isSet(self.cards[x], self.cards[y], self.cards[z]):
+            for c in sorted([x, y, z], reverse=True):
+                self.cards.pop(c)
+            return True
+        else:
+            return False
+
+    def hasSet(self):
+        return 
+
+    def listCards(self):
+        return 
+
+
+class Player:
+
+    def __init__(self):
+        self.points = 0
+        self.name = ''
+
+    def addPoint(self):
+        return 
+
+    def losePoint(self):
+        return 
+
+class Game:
+
+    def __init__(self):
+        self.cards = []
+        self.board = Board()
+        self.players = []
+        for n in Number:
+            for c in Color:
+                for f in Fill:
+                    for s in Shape:
+                        self.cards.append(Card(n, f, c, s))
+
+        random.shuffle(self.cards)
+
+    def start(self):
+        for x in range(0,12):
+            self.board.placeCard(self.cards.pop())
+
+        while True:
+
+            for c in self.board.cards:
+                print(c)
+
+            v = input("Find a set: ")
+            if v == 'q': 
+                self.quit()
+
+            if self.board.removeSet(v.split()):
+                print("SET!")
+                self.board.placeCard(self.cards.pop())
+                self.board.placeCard(self.cards.pop())
+                self.board.placeCard(self.cards.pop())
+            else:
+                print("NO SET!")
+
+        return 
+
+    def quit(self):
+        sys.exit()
+
+    def end(self):
+        return 
+
+game = Game()
+game.players.append(Player())
+game.start()
