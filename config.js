c2250e8300259423b4d5f41d2e35c158bca3d3c1
Eric Bloch <eric.bloch@udemy.com>
2015-11-04 00:52:29 -0800

    updates

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ System.config({
     "firebase": "github:firebase/firebase-bower@2.3.1",
     "jquery": "github:components/jquery@2.1.4",
     "lodash-node": "npm:lodash-node@3.10.1",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.92",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.92",
     "underscore": "npm:underscore@1.8.3",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
