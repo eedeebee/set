a87530821add7de340bfcd7178abf17d12748e83
Eric Bloch <eric.bloch@udemy.com>
2020-08-27 22:57:30 -0700

    rebuild

--- jspm_packages/github/firebase/firebase-bower@7.17.2.js
+++
@@ -1,3 +0,0 @@
-define(["github:firebase/firebase-bower@7.17.2/firebase.js"], function(main) {
-  return main;
-});
---
+++ jspm_packages/github/firebase/firebase-bower@7.19.1.js
@@ -0,0 +1,3 @@
+define(["github:firebase/firebase-bower@7.19.1/firebase.js"], function(main) {
+  return main;
+});
